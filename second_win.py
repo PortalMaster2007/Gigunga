27e605d3e894b8fb304f1337efd4c281b0d0d843
PortalMaster2007 <artm-1982@bk.ru>
2025-04-06 11:00:18 +0300

    update

--- second_win.py
+++ second_win.py
@@ -1,4 +1,5 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer, QTime
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont
 from PyQt5.QtWidgets import (
         QApplication, QWidget,
         QHBoxLayout, QVBoxLayout,
@@ -15,6 +16,46 @@ class TestWin(QWidget):
         self.initUI()
         self.connects()
         self.show()
+
+    def timer_test(self):
+        self.timer.timeout.connect(self.timer1Event)
+        self.timer.start(1000)
+
+    def timer_sits(self):
+        time = QTime(0, 0, 30)
+        self.timer.timeout.connect(self.timer2Event)
+        self.timer.start(1500)
+
+    def timer_test(self):
+        global time
+        time = QTime(0, 1, 0)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer3Event)
+        self.timer.start(1000)
+
+    def timer_final(self):
+        time = QTime(0, 1, 0)
+        self.timer.timeout.connect(self.timer3Event)
+
+    def timer1Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.text_timer.setText(time.toString("hh,mm,ss"))
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+        if time.toString("hh,mm,ss") == "00:00:00":
+            self.timer.stop()
+
+    def timer2Event(self):
+        self.text_timer.setText(time.toString("hh,mm,ss")[6:8])
+
+    def timer3Event(self):
+        if int(time.toString("hh,mm,ss")[6:8]) >= 45:
+            self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+        elif int(time.toString("hh,mm,ss")[6:8]) <= 15:
+            self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+        else:
+            self.text_timer.setStyleSheet("color: rgb(0,0,0)")
 
     def set_appear(self):
         self.setWindowTitle('Здоровье')
@@ -58,11 +99,16 @@ class TestWin(QWidget):
         self.h_line.addLayout(self.l_line) 
         self.h_line.addLayout(self.r_line)       
         self.setLayout(self.h_line)
-        
 
-    def next_click(self):
+   def next_click(self):
         self.tw = FinalWin()
         self.hide()
 
     def connects(self):
         self.btn_next.clicked.connect(self.next_click)
+        self.btn_test1.clicked.connect(self.timer_test)
+        self.btn_test1.clicked.connect(self.timer_test)
+        self.btn_test2.clicked.connect(self.timer_sits)
+        self.btn_test3.clicked.connect(self.timer_final)
+
+
